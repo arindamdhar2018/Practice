8d056a99d75218326305ffba0b6bfe760eca9b23
Arindam Dhar <arindam.dhar06@gmail.com>
2021-09-02 19:12:58 +0530

    Update mainFile.py

--- mainFile.py
+++ mainFile.py
@@ -2,16 +2,6 @@
 Fist line
 Second Line
 Third Line Removed
-But Strated 4th Line from branch
-5th line added from branch
-Done from main
 
-X from branch
-
-A B from main
-C from main
-add from second branch
-add from Third branch
-mfrom  Third branch
 BCDfrom Third branch
 xyz added
